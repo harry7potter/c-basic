81456153ce88c70c6c4530509717954339aba9fb
harry7potter <55805833+harry7potter@users.noreply.github.com>
2019-10-01 22:26:29 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+    #include<iostream>
+     
+    using namespace std;
+     
+    // Creating class
+     
+    class Message
+    {
+      public:
+     
+        void display() {
+          cout << "Hello World\n";
+        }
+    };
+     
